e708f85b0762f68a6f9b3afd5c076a19df2f8da9
Chien Nguyen <chien.nm196@gmail.com>
2024-04-27 15:47:40 +0700

    Move logic stream to torrent manager

--- desktop/server/handler/handler_stream.go
+++ desktop/server/handler/handler_stream.go
@@ -2,10 +2,7 @@ package handler
 
 import (
 	"net/http"
-	"time"
 
-	"github.com/friendsofgo/errors"
-	"github.com/gabriel-vasile/mimetype"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -19,22 +16,5 @@ func (h *Handler) Stream(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
-	file, err := h.torManager.GetFile(infoHash, fileIndex)
-	if err != nil {
-		return errors.Wrap(err, "get file")
-	}
-
-	file.Download()
-	reader := file.NewReader()
-	reader.SetResponsive()
-
-	mime, err := mimetype.DetectReader(reader)
-	if err != nil {
-		return errors.Wrap(err, "detect mime type")
-	} else {
-		w.Header().Set("Content-Type", mime.String())
-	}
-
-	http.ServeContent(w, r, file.DisplayPath(), time.Time{}, reader)
-	return nil
+	return h.torManager.StreamFile(w, r, infoHash, fileIndex)
 }
--- desktop/torrent/manager_get.go
+++ desktop/torrent/manager_get.go
@@ -2,6 +2,11 @@ package torrent
 
 import (
 	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/friendsofgo/errors"
+	"github.com/gabriel-vasile/mimetype"
 
 	"github.com/anacrolix/torrent"
 	"github.com/anacrolix/torrent/metainfo"
@@ -22,4 +27,25 @@ func (m *Manager) GetTorrent(infoHash InfoHash) (*torrent.Torrent, bool) {
 	}
 	<-tor.GotInfo()
 	return tor, true
+}
+
+func (m *Manager) StreamFile(w http.ResponseWriter, r *http.Request, infoHash InfoHash, fileIndex int) error {
+	file, err := m.GetFile(infoHash, fileIndex)
+	if err != nil {
+		return errors.Wrap(err, "get file")
+	}
+
+	file.Download()
+	reader := file.NewReader()
+	reader.SetResponsive()
+
+	mime, err := mimetype.DetectReader(reader)
+	if err != nil {
+		return errors.Wrap(err, "detect mime type")
+	} else {
+		w.Header().Set("Content-Type", mime.String())
+	}
+
+	http.ServeContent(w, r, file.DisplayPath(), time.Time{}, reader)
+	return nil
 }
