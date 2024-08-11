ed8ee9c32c60a6c1c9f14ac7cbc173a28eaa14b8
Chien Nguyen <chien.nm196@gmail.com>
2024-08-11 16:26:58 +0700

    Add search view

--- phimtor_app/lib/views/home_view.dart
+++ phimtor_app/lib/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:phimtor_app/services/phimtor/phimtor_service.dart'
     as phimtor_api;
@@ -8,6 +9,94 @@ import 'package:phimtor_openapi_client/api.dart';
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Phim Tor"),
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SearchSection(),
+              SizedBox(height: 32),
+              MoviesSection(),
+              SizedBox(height: 32),
+              TVSeriesSection(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchSection extends StatefulWidget {
+  const SearchSection({super.key});
+
+  @override
+  State<SearchSection> createState() => _SearchSectionState();
+}
+
+class _SearchSectionState extends State<SearchSection> {
+  final _searchController = TextEditingController();
+
+  Future<List<ModelShow>> _loadSearch(int page, int pageSize) async {
+    final query = _searchController.text;
+    if (query.isEmpty) {
+      return [];
+    }
+    final resp = await phimtor_api.PhimtorService()
+        .defaultApi
+        .searchShows(_searchController.text, page: page);
+    if (resp == null) {
+      throw Exception("Null response");
+    }
+    return resp.shows;
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Search",
+          style: Theme.of(context).textTheme.headlineLarge,
+        ),
+        const SizedBox(height: 16),
+        // a search text box with a button to search
+        CupertinoSearchTextField(
+          controller: _searchController,
+          onSubmitted: (query) {
+            if (query.isEmpty) {
+              return;
+            }
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => ShowsGridView(
+                title: "Search results for '$query'",
+                loadMore: _loadSearch,
+              ),
+            ));
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class MoviesSection extends StatelessWidget {
+  const MoviesSection({super.key});
+
   Future<List<ModelShow>> _loadMovies(int page, int pageSize) async {
     final resp = await phimtor_api.PhimtorService()
         .defaultApi
@@ -17,6 +106,63 @@ class HomeView extends StatelessWidget {
     }
     return resp.shows;
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Movies",
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            const SizedBox(width: 8),
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => ShowsGridView(
+                    title: "Movies",
+                    loadMore: _loadMovies,
+                  ),
+                ));
+              },
+              label: const Text("Load more"),
+              icon: const Icon(Icons.arrow_forward),
+              iconAlignment: IconAlignment.end,
+            ),
+          ],
+        ),
+        const SizedBox(height: 16),
+        SizedBox(
+          height: ShowsList.minHeight,
+          child: FutureBuilder(
+            future: phimtor_api.PhimtorService()
+                .defaultApi
+                .listShows(page: 1, pageSize: 10, type: ShowType.movie),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return Center(child: Text("Error: ${snapshot.error}"));
+              }
+
+              final response = snapshot.data as ListShowsResponse;
+              return ShowsList(shows: response.shows);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TVSeriesSection extends StatelessWidget {
+  const TVSeriesSection({super.key});
 
   Future<List<ModelShow>> _loadSeries(int page, int pageSize) async {
     final resp = await phimtor_api.PhimtorService()
@@ -30,107 +176,51 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Phim Tor"),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    "Movies",
-                    style: Theme.of(context).textTheme.headlineLarge,
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              "TV Series",
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            const SizedBox(width: 8),
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => ShowsGridView(
+                    title: "TV Series",
+                    loadMore: _loadSeries,
                   ),
-                  const SizedBox(width: 8),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ShowsGridView(
-                          title: "Movies",
-                          loadMore: _loadMovies,
-                        ),
-                      ));
-                    },
-                    label: const Text("Load more"),
-                    icon: const Icon(Icons.arrow_forward),
-                    iconAlignment: IconAlignment.end,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                height: ShowsList.minHeight,
-                child: FutureBuilder(
-                  future: phimtor_api.PhimtorService()
-                      .defaultApi
-                      .listShows(page: 1, pageSize: 10, type: ShowType.movie),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    if (snapshot.hasError) {
-                      return Center(child: Text("Error: ${snapshot.error}"));
-                    }
+                ));
+              },
+              label: const Text("Load more"),
+              icon: const Icon(Icons.arrow_forward),
+              iconAlignment: IconAlignment.end,
+            ),
+          ],
+        ),
+        const SizedBox(height: 16),
+        SizedBox(
+          height: ShowsList.minHeight,
+          child: FutureBuilder(
+            future: phimtor_api.PhimtorService()
+                .defaultApi
+                .listShows(page: 1, pageSize: 10, type: ShowType.series),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return Center(child: Text("Error: ${snapshot.error}"));
+              }
 
-                    final response = snapshot.data as ListShowsResponse;
-                    return ShowsList(shows: response.shows);
-                  },
-                ),
-              ),
-              const SizedBox(height: 32),
-              Row(
-                children: [
-                  Text(
-                    "TV Series",
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                  const SizedBox(width: 8),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ShowsGridView(
-                          title: "TV Series",
-                          loadMore: _loadSeries,
-                        ),
-                      ));
-                    },
-                    label: const Text("Load more"),
-                    icon: const Icon(Icons.arrow_forward),
-                    iconAlignment: IconAlignment.end,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                height: ShowsList.minHeight,
-                child: FutureBuilder(
-                  future: phimtor_api.PhimtorService()
-                      .defaultApi
-                      .listShows(page: 1, pageSize: 10, type: ShowType.series),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    if (snapshot.hasError) {
-                      return Center(child: Text("Error: ${snapshot.error}"));
-                    }
-
-                    final response = snapshot.data as ListShowsResponse;
-                    return ShowsList(shows: response.shows);
-                  },
-                ),
-              ),
-            ],
+              final response = snapshot.data as ListShowsResponse;
+              return ShowsList(shows: response.shows);
+            },
           ),
         ),
-      ),
+      ],
     );
   }
 }
