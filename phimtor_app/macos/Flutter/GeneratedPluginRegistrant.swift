3826b7a9b580b4a565539a27ae64360672a341b8
Chien Nguyen <chien.nm196@gmail.com>
2024-08-17 21:33:23 +0700

    Add config for firebase

--- phimtor_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ phimtor_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import media_kit_libs_macos_video
 import media_kit_video
 import package_info_plus
@@ -14,6 +15,7 @@ import shared_preferences_foundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   MediaKitLibsMacosVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosVideoPlugin"))
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
