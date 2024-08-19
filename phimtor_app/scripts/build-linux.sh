0fec8789fbcbd4bf5a8f1e2e0c7c918f17aee025
Chien Nguyen <chien.nm196@gmail.com>
2024-08-20 00:45:29 +0700

    Build linux

---
+++ phimtor_app/scripts/build-linux.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+set -xe
+
+readonly VERSION=${1:-0.0.1}
+readonly BUILD_NUMBER=${2:-0}
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+WORKING_DIR=$(cd "$SCRIPT_DIR"/.. && pwd)
+APP_DIR="$WORKING_DIR"/build/Phimtor.AppDir
+
+
+cd "$WORKING_DIR"
+
+flutter build linux --release \
+    --build-number=$BUILD_NUMBER \
+    --build-name=$VERSION \
+    --dart-define=API_URL=$SERVER_ADDR 
+
+mkdir -p "$APP_DIR"
+rm -rf "$APP_DIR"/*
+
+cp -r build/linux/x64/release/bundle//* "$APP_DIR"/
+cp assets/icon/icon.png "$APP_DIR"/
+
+cat > "$APP_DIR"/AppRun <<EOL
+#!/bin/sh
+cd "\$(dirname "\$0")"
+exec ./phimtorapp
+EOL
+
+chmod +x "$APP_DIR"/AppRun
+
+
+cat > "$APP_DIR"/PhimTor.desktop <<EOL
+[Desktop Entry]
+Name=Phim Tor
+Exec=phimtorapp
+Icon=icon
+Type=Application
+Terminal=false
+Categories=AudioVideo;Player;
+EOL
+
+if [ -f "appimagetool-x86_64.AppImage" ]; then
+    echo "appimagetool-x86_64.AppImage already exists"
+else
+    wget "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+    chmod a+x appimagetool-x86_64.AppImage 
+fi
+
+./appimagetool-x86_64.AppImage "$APP_DIR" "build/PhimTor-x86_64.AppImage"
+
