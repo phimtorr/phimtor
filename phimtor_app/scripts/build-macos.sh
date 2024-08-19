694f4beb64ef26af0e4851a24f96dfb09ff56fb8
Chien Nguyen <chien.nm196@gmail.com>
2024-08-20 02:04:44 +0700

    Support other platforms

---
+++ phimtor_app/scripts/build-macos.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -xe
+
+readonly VERSION=${1:-0.0.1}
+readonly BUILD_NUMBER=${2:-0}
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+WORKING_DIR=$(cd "$SCRIPT_DIR"/.. && pwd)
+
+flutter build macos --release \
+    --build-number=$BUILD_NUMBER \
+    --build-name=$VERSION \
+    --dart-define=API_URL=$SERVER_ADDR 
