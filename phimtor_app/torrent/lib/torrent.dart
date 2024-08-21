f44ee3aa6801b010a2c039644eedd07d8bf2674c
Nguyen Minh Chien <chienminhnguyen196@gmail.com>
2024-08-22 01:40:19 +0700

    Corrent lib name on windows

--- phimtor_app/torrent/lib/torrent.dart
+++ phimtor_app/torrent/lib/torrent.dart
@@ -13,31 +13,25 @@ const String _libName = 'torrent';
 
 final ffi.DynamicLibrary _dylib = () {
   if (Platform.isMacOS || Platform.isIOS) {
-    // Add from here...
     if (Platform.environment.containsKey('FLUTTER_TEST')) {
       return ffi.DynamicLibrary.open('build/macos/Build/Products/Debug'
           '/$_libName/$_libName.framework/$_libName');
     }
-    // ...to here.
     return ffi.DynamicLibrary.open('$_libName.framework/$_libName');
   }
   if (Platform.isAndroid || Platform.isLinux) {
-    // Add from here...
     if (Platform.environment.containsKey('FLUTTER_TEST')) {
       return ffi.DynamicLibrary.open(
           'build/linux/x64/debug/bundle/lib/lib$_libName.so');
     }
-    // ...to here.
     return ffi.DynamicLibrary.open('lib$_libName.so');
   }
   if (Platform.isWindows) {
-    // Add from here...
     if (Platform.environment.containsKey('FLUTTER_TEST')) {
       return ffi.DynamicLibrary.open(p.canonicalize(
-          p.join(r'build\windows\runner\Debug', '$_libName.dll')));
+          p.join(r'build\windows\runner\Debug', 'lib$_libName.dll')));
     }
-    // ...to here.
-    return ffi.DynamicLibrary.open('$_libName.dll');
+    return ffi.DynamicLibrary.open('lib$_libName.dll');
   }
   throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
 }();
