d69b6de655249cb1f9e31eb95e612cb598afc8c7
Nguyen Minh Chien <chienminhnguyen196@gmail.com>
2024-09-13 22:31:51 +0700

    Align GitHub actions (#28)
    * Remove build macos dmg file
    
    * Remove build desktop

--- phimtor_app/scripts/build-macos.sh
+++ phimtor_app/scripts/build-macos.sh
@@ -21,27 +21,3 @@ flutter build macos --release \
 APP_DIR="${WORKING_DIR}/build/macos/Build/Products/Release/PhimTor.app"
 
 productbuild --component "$APP_DIR" /Applications "$WORKING_DIR/build/PhimTor-${VERSION}.pkg"
-
-# DMG file 
-mkdir -p "$WORKING_DIR/build/dmg"
-cp -r "$APP_DIR" "$WORKING_DIR/build/dmg/PhimTor.app"
-
-DMG_FILE_NAME="PhimTor-${VERSION}.dmg"
-VOLUME_NAME="PhimTor Installer"
-SOURCE_FOLDER="$WORKING_DIR/build/dmg"
-DEST_FILE="$WORKING_DIR/build/$DMG_FILE_NAME"
-APP_NAME="PhimTor"
-
-[[ -f "${DEST_FILE}" ]] && rm "${DEST_FILE}"
-
-create-dmg \
-    --volname "${VOLUME_NAME}" \
-    --volicon "$WORKING_DIR/assets/icon/icon.icns" \
-    --window-pos 200 120 \
-    --window-size 800 400 \
-    --icon-size 100 \
-    --icon "${APP_NAME}.app" 200 190 \
-    --hide-extension "${APP_NAME}.app" \
-    --app-drop-link 600 185 \
-    "${DEST_FILE}" \
-    "${SOURCE_FOLDER}"
